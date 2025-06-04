5181056e1f03edab17b236cd79d3ed253db50864
Maksym Khalin <xadmax@gmail.com>
2025-06-04 17:28:48 +0200

    Rename fetchQuestions API function

--- 03_Client/src/utils/api.js
+++ 03_Client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const mapper = questions =>
     questions.map(({ _id: id, ...props }) => ({ id, ...props }));
 
-export const fetchQuesions = amount =>
+export const fetchQuestions = amount =>
     axios
     .get(`http://${process.env.GEO_SRV}/questions?number=${amount}`)
     .then(({ data }) => mapper(data))
